29ecbc2de2a974580dbc02c2f82199be3301e69a
hagaitski <i.haga629@gmail.com>
2023-03-19 05:11:02 +0900

    rm: feature code

--- src/models/store.ts
+++ src/models/store.ts
@@ -1,11 +1,6 @@
-import { collection, orderBy, query, QueryDocumentSnapshot, SnapshotOptions } from "@firebase/firestore";
+import { QueryDocumentSnapshot, SnapshotOptions } from "@firebase/firestore";
 import { z } from "zod";
-import { db } from "../lib/firestore";
-import { personaSchema } from "./schema";
-
-export type PersonasDocument = z.infer<typeof personaSchema>;
-
-class ZodSchemaConverter<T> {
+export class ZodSchemaConverter<T> {
   constructor(private schema: z.ZodSchema<T>) {}
 
   toFirestore(data: T) {
@@ -17,73 +12,3 @@ class ZodSchemaConverter<T> {
     return this.schema.parse(data);
   }
 }
-
-export const personasCollection = collection(db, "/personas").withConverter<PersonasDocument>(
-  new ZodSchemaConverter(personaSchema),
-);
-
-const playerDocumentSchema = z.object({
-  personaId: z.union([z.string(), z.undefined()]),
-  name: z.union([z.string(), z.undefined()]),
-});
-
-export type PlayerDocument = z.infer<typeof playerDocumentSchema>;
-
-export const playerCollection = (roomId: string) =>
-  collection(db, `/rooms/${roomId}/players`).withConverter<PlayerDocument>(
-    new ZodSchemaConverter(playerDocumentSchema),
-  );
-
-export const roomDocumentSchema = z.object({
-  phase: z.union([z.literal("prepare"), z.literal("game")]),
-});
-
-export type RoomDocument = z.infer<typeof roomDocumentSchema>;
-
-export const roomCollection = collection(db, `/rooms/`).withConverter<RoomDocument>(
-  new ZodSchemaConverter(roomDocumentSchema),
-);
-
-export const translatableSchema = z.object({
-  original: z.string(),
-  translated: z.union([z.string(), z.undefined()]),
-});
-
-export const roomActionsDocumentSchema = z.union([
-  z.object({
-    type: z.literal("INIT_POTS"),
-    payload: z.object({
-      pots: z.array(
-        z.object({
-          id: z.string(),
-        }),
-      ),
-    }),
-    timestamp: z.number(),
-  }),
-  z.object({
-    type: z.literal("PUT_INGREDIENT"),
-    payload: z.object({
-      potId: z.string(),
-      ingredient: translatableSchema,
-    }),
-    timestamp: z.number(),
-  }),
-  z.object({
-    type: z.literal("LOOK_INTO_POT"),
-    payload: z.object({
-      potId: z.string(),
-      imageUrl: z.string(),
-    }),
-    timestamp: z.number(),
-  }),
-]);
-
-export type RoomActionsDocument = z.infer<typeof roomActionsDocumentSchema>;
-
-export const roomActionsCollection = (roomId: string) =>
-  collection(db, `/rooms/${roomId}/actions`).withConverter<RoomActionsDocument>(
-    new ZodSchemaConverter(roomActionsDocumentSchema),
-  );
-
-export const roomActionsQueryBase = (roomId: string) => query(roomActionsCollection(roomId), orderBy("timestamp"));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,9 @@
-import { playerCollection, roomCollection } from "@/models/store";
-import { doc, setDoc } from "@firebase/firestore";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Home() {
-  const router = useRouter();
-  const [loading, setLoading] = useState<boolean>(false);
   return (
     <main>
       <h1>Hello World</h1>
-      {loading ? (
-        <div>loading</div>
-      ) : (
-        <button
-          onClick={async () => {
-            const room = doc(roomCollection);
-            const player = doc(playerCollection(room.id), "testes");
-            setLoading(true);
-            await Promise.all([setDoc(player, {}), setDoc(room, { phase: "prepare" })]);
-            router.push(`/rooms/${room.id}`);
-          }}
-        >
-          start
-        </button>
-      )}
     </main>
   );
 }
